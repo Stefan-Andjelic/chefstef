1893990fa4d57c682543aed025cabb6931addb1e
Stefan <stefanandjelic8@gmail.com>
2021-12-05 18:31:33 -0600

    base dir

--- chefstef2/settings.py
+++ chefstef2/settings.py
@@ -16,7 +16,7 @@ import os
 import braintree
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
